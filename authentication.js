4c1711398ddd9882b05a73886cda92d1ad1db963
simran verma <vermasimran327@gmail.com>
2025-09-08 22:35:12 +0530

    Add files via upload

---
+++ authentication.js
@@ -0,0 +1,29 @@
+const JWT = require('jsonwebtoken');
+// const { create } = require('../models/user');
+
+
+const secret = "Zeal@1234";
+
+function createTokenForUser(user){
+    const payload = {
+        _id: user._id,
+        fullName: user.fullName,
+        email:user.email,
+        profileImageURL: user.profileImageURL,
+        role:user.role,
+    };
+
+    const token = JWT.sign(payload, secret);
+    return token;
+}
+
+
+function validateToken(token){
+    const payload = JWT.verify(token,secret);
+    return payload;
+}
+
+module.exports = {
+    createTokenForUser,
+    validateToken,
+};
---
+++ blog.js
@@ -0,0 +1,67 @@
+
+const User = require("../models/user");
+const { Router } = require("express");
+const multer = require('multer');
+const path = require("path");
+
+const Blog = require("../models/blog");
+const Comment = require("../models/comment");
+
+const router = Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.resolve(`./public/uploads/`));
+    },
+    filename: function (req, file, cb) {
+        const filename = `${Date.now()}-${file.originalname}`;
+        cb(null, filename);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+router.get("/add-new", (req, res) => {
+    return res.render("addblog", {
+        user: req.user,
+    });
+});
+
+router.get("/:id", async(req,res)=>{
+
+    const blog = await  Blog.findById(req.params.id).populate("createdBy");
+    const comments = await Comment.find({blogId: req.params.id}).populate("createdBy").sort({createdAt:-1});
+    return res.render("blog",{
+        user:req.user,
+        blog,
+        comments,
+    });
+});
+  
+
+
+router.post('/comment/:blogId',async (req,res)=>{
+    await Comment.create({
+        content: req.body.content,
+        blogId: req.params.blogId,
+        createdBy:req.user._id,
+
+
+    });
+    // console.log(comments);
+    return res.redirect(`/blog/${req.params.blogId}`);
+});
+
+
+router.post("/", upload.single("coverImage"), async (req, res) => {
+  const { title, body } = req.body;
+  const blog = await Blog.create({
+    body,
+    title,
+    createdBy: req.user._id,
+    coverImageURL: `/uploads/${req.file.filename}`,
+  });
+  return res.redirect(`/blog/${blog._id}`);
+});
+
+module.exports = router;
